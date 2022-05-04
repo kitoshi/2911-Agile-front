35871dc5824db76f8cced3292d7e72c7cafecc1e
Tyler <tgrande@my.bcit.ca>
2022-05-04 10:21:51 -0700

    pushing to new repository

--- src/app.js
+++ src/app.js
@@ -4,15 +4,11 @@ async function makeFetchtoBackEndToGetData() {
     return json
   }
 
+
 const allMovies = document.getElementById("display_movies")
 let idTracker = 0
 
-async function main() {
-    const listOfMovies = await makeFetchtoBackEndToGetData()
-    await displayAllMovies(listOfMovies)
-
-  }
-
+  
 function displayAllMovies(listOfMovies){
     for (let numberOfMovies of listOfMovies) {
         const titleTag = document.createElement('p');
@@ -20,12 +16,13 @@ function displayAllMovies(listOfMovies){
         const ratingTag = document.createElement('p');
         const movieDiv = document.createElement('div');
         const imgTag = document.createElement('img');
+        const button = document.createElement('button');
 
         movieDiv.id = idTracker
 
         idTracker = idTracker + 1
         for (let object in numberOfMovies) {
-            console.log(object)
+            movieDiv.append(button)
             if ( object === 'title' ) {
                 noSpecialCharacters = numberOfMovies.title.replace(/[^a-zA-Z0-9 ]/g, '')
                 titleTag.textContent = numberOfMovies.title
@@ -48,4 +45,39 @@ function displayAllMovies(listOfMovies){
         }
     
 }
+
+allMovies.addEventListener('click', (evt) => {
+    console.log(evt.target)
+    let button = document.createElement('button')
+    if (evt.target == button){
+        console.log(evt.target.parentElement)
+    }
+ 
+
+
+    
+})
+// async function fetchCrud(listOfMovies) {
+//     let movieID = 'https://agile-2199.uw.r.appspot.com/api' + 
+//     const response = await fetch(movieID, {
+//         method: 'PUT'
+
+//     })
+// }
+
+
+// function incrementVote() {
+//     database?.updateOne()
+// }
+
+
+
+async function main() {
+
+
+    const listOfMovies = await makeFetchtoBackEndToGetData()
+    await displayAllMovies(listOfMovies)
+
+  }
+
 main()
